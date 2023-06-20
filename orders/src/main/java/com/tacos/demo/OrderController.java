646658baf92219c0e497485908a577d89f209e20
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2023-06-20 11:06:13 +0200

    fix request

--- orders/src/main/java/com/tacos/demo/OrderController.java
+++ orders/src/main/java/com/tacos/demo/OrderController.java
@@ -13,34 +13,33 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.Metrics;
-import jakarta.servlet.http.HttpServletRequest;
-
 @RestController
 
 public class OrderController {
 
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(OrderController.class);
-    @RequestMapping("/{tacos}")
-    public String handleGetRequest(HttpServletRequest request, @PathVariable String tacos) throws IOException {
+
+    @RequestMapping("/orderTacos/{numTacos}")
+    public String handleGetRequest(HttpServletRequest request, @PathVariable String numTacos) throws IOException {
         Counter orders = Metrics.counter("orders");
         log.info("Received HTTP GET request. Path: {}, Remote Address: {}", request.getRequestURL(), request.getRemoteAddr());
-        int tacosInt = (tacos != null) ? Integer.parseInt(tacos) : 1;
-        getOrders(tacosInt);
+        int tacosInt;
+        try {
+            tacosInt = Integer.parseInt(numTacos);
+        } catch (NumberFormatException e) {
+            log.warn("could not parse number from string '{}'. Using 1 instead.", numTacos);
+            tacosInt = 1;
+        }
+        String orderResponse = getOrders(tacosInt);
         orders.increment(tacosInt);
-        return "order received";
+        return "order received: " + orderResponse;
     }
 
-    public String getOrders(int tacos) throws IOException {     
-        Counter successfulDeliveries = Metrics.counter("deliveries","success","true");
-        Counter unsuccessfulDeliveries = Metrics.counter("deliveries","success","false");
-        URL url = new URL ("http://deliveries:8081/");
-        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+    public String getOrders(int tacos) throws IOException {
+        Counter successfulDeliveries = Metrics.counter("deliveries", "success", "true");
+        Counter unsuccessfulDeliveries = Metrics.counter("deliveries", "success", "false");
+        URL url = new URL("http://deliveries:8081/");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         String response = new String(con.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
         if (response.equals("true")) {
